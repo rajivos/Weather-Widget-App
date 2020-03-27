1cbced1b319b95062d41855a25a0392890413b44
Rajiv Khushiram <31241377+Rajiv-Khushiram@users.noreply.github.com>
2020-03-28 01:07:51 +1100

    minimized test functions time execution

--- weather-app-pwc/src/App.test.js
+++ weather-app-pwc/src/App.test.js
@@ -1,24 +1,26 @@
 import { getForecast } from "./Components/APIs/getForecast";
 import { getCoordinatesByName } from "./Components/APIs/getCoordinatesByName";
 
-describe("#getForecast() ", () => {
-  describe("#getUser() using async/await", () => {
-    it("Returns Schedule for given latitude and longitude", async () => {
-      await getForecast(-37.805, 144.962, "metric").then(res => {
-        const { city_name } = res.data
-        expect(res.status).toEqual(200);
-        expect(city_name).toEqual("Melbourne");
-      });
-    });
-  });
-});
-
 describe("#getCoordinatesByName() ", () => {
   it("Returns coordinates for given name", () => {
-    return getCoordinatesByName("Mauritius").then(res => {
+     getCoordinatesByName("Mauritius").then(res => {
       const { formatted } = res.data.results[0]
       expect(res.status).toEqual(200);
       expect(formatted).toEqual("Mauritius");
     });
   });
 });
+
+describe("#getForecast() ", () => {
+    it("Returns Schedule for given latitude and longitude",() => {
+      getForecast(-37.805, 144.962, "metric").then(res => {
+        const { city_name } = res.data
+        expect(res.status).toEqual(200);
+        expect(city_name).toEqual("Melbourne");
+      });
+    });
+});
+
+
+
+
